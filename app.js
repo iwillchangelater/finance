701601089194904e20621b1082d5f680e3db203c
iwillchangelater <78591921+iwillchangelater@users.noreply.github.com>
2021-03-19 23:15:07 +0800

    sanhuugiin dotorh uildeluud bolson

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var uiController = (function() {
         inputType: ".add__type",
         inputDescription: ".add__description",
         inputValue: ".add__value",
-        addBtn: ".add__btn"
+        addBtn: ".add__btn",
+        incomeList: ".income__list",
+        expenseList: ".expenses__list"
     };
     return {
         getDOMList: function() {
@@ -14,12 +16,29 @@ var uiController = (function() {
             return {
                 type: document.querySelector(DOMList.inputType).value,
                 description: document.querySelector(DOMList.inputDescription).value,
-                value: document.querySelector(DOMList.inputValue).value
+                value: parseInt(document.querySelector(DOMList.inputValue).value)
             }
 
+        },
+        addList: function(item, type) {
+            if (type == "inc") {
+                html = '<div class="item clearfix" id="income-' + item.id + '"><div class="item__description">' + item.description + '</div><div class="right clearfix"><div class="item__value">' + item.value + '</div><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                list = DOMList.incomeList;
+            } else {
+                html = ' <div class="item clearfix" id="expense-' + item.id + '"> <div class="item__description">' + item.description + '</div><div class="right clearfix"> <div class="item__value">' + item.value + '</div> <div class="item__percentage">21%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';;
+                list = DOMList.expenseList;
+            }
+            document.querySelector(list).insertAdjacentHTML('beforeend', html);
+        },
+        clearField: function() {
+            var fields = document.querySelectorAll(DOMList.inputDescription + "," + DOMList.inputValue);
+            var fieldsArr = Array.prototype.slice.call(fields);
+            fieldsArr.forEach(function(el) {
+                el.value = "";
+            });
+            fieldsArr[0].focus();
         }
     }
-
 })();
 // санхүүгийн контроллер 
 var fanincialController = (function() {
@@ -34,6 +53,14 @@ var fanincialController = (function() {
         this.description = description;
         this.value = value;
     }
+    var calcTotal = function(type) {
+        var sum = 0;
+        data.items[type].forEach(function(el) {
+            sum += el.value;
+        })
+        data.totals[type] = sum;
+
+    }
     var data = {
         items: {
             inc: [],
@@ -42,7 +69,9 @@ var fanincialController = (function() {
         totals: {
             inc: 0,
             exp: 0
-        }
+        },
+        tusuv: 0,
+        huvi: 0
     }
     return {
         addItem: function(type, desc, val) {
@@ -59,13 +88,25 @@ var fanincialController = (function() {
             } else {
                 item = new expense(id, desc, val);
             }
-            data.items[type].push(item)
+            data.items[type].push(item);
+            return item;
         },
-        seedata: function() {
-            return data;
+        seeData: function() {
+            console.log(data);
         },
-        deldata: function() {
-            return delete data.items["inc"]["1"];
+        total: function() {
+            calcTotal('inc');
+            calcTotal('exp');
+            data.tusuv = data.totals.inc - data.totals.exp;
+            data.huvi = Math.round((data.totals.exp / data.totals.inc) * 100);
+        },
+        tusuviigAvah: function() {
+            return {
+                tusuv: data.tusuv,
+                huvi: data.huvi,
+                totalInc: data.totals.inc,
+                totalExp: data.total.exp
+            }
         }
     }
 })();
@@ -74,12 +115,18 @@ var connectionController = (function(uiController, fanincialController) {
     var ctrlAddItem = function() {
         var input = uiController.getInput();
         // 1 орлого зарлагаа аван.
-        fanincialController.addItem(input.type, input.description, input.value);
-        console.log("nemegdsen");
-        // 2 орлого зарлагаа санхүүгийн контроллэрт дамжүүлна
-        // 3 мэдээллийг харагдах газарт нь гаргана.
-        // 4 төсөв тооцоолох 
-        // 5 эцсийн үлдэгдэл    
+        if (input.description !== "" && !isNaN(input.value)) {
+            var item = fanincialController.addItem(input.type, input.description, input.value);
+            console.log("nemegdsen");
+            uiController.addList(item, input.type);
+            uiController.clearField();
+            // 2 орлого зарлагаа санхүүгийн контроллэрт дамжүүлна
+            // 3 мэдээллийг харагдах газарт нь гаргана.
+            // 4 төсөв тооцоолох 
+            // 5 эцсийн үлдэгдэл    
+            fanincialController.total();
+
+        }
     };
     var setupEventListeners = function() {
         var DOM = uiController.getDOMList();
